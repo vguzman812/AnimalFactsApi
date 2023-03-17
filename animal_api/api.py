048d13246f2b06c5c8d4939d77c3a24795866e31
vincent <vguzman812@gmail.com>
2023-03-17 14:11:30 -0400

    Change pipfile packages.

--- animal_api/api.py
+++ animal_api/api.py
@@ -1,8 +1,10 @@
-from constants import PROJECT_ROOT, ANIMAL_FACTS_DATABASE
-from resources.facts_resources import FactsResource, FACTS_ENDPOINT
-from database import db
 from flask import Flask
 from flask_restful import Api
+
+from constants import PROJECT_ROOT, ANIMAL_FACTS_DATABASE
+from database import db
+from resources.facts_resources import FactsResource, FACTS_ENDPOINT
+
 import logging
 import sys
 from os import path
@@ -27,15 +29,15 @@ def create_app(db_location):
         handlers=[logging.FileHandler("animal_api.log"), logging.StreamHandler()],
     )
 
-    current_app = Flask(__name__)
-    current_app.config["SQLALCHEMY_DATABASE_URI"] = db_location
-    db.init_app(current_app)
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_location
+    db.init_app(app)
 
-    api = Api(current_app)
+    api = Api(app)
     api.add_resource(FactsResource, FACTS_ENDPOINT, f"{FACTS_ENDPOINT}/<id>")
-    return current_app
+    return app
 
 
 if __name__ == "__main__":
     app = create_app(f"sqlite:////{PROJECT_ROOT}/{ANIMAL_FACTS_DATABASE}")
-    app.run(debug=True, host="localhost", port=6789)
+    app.run(debug=True)
